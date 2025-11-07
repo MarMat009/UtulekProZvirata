363844e85992889a2ecc60493e77ac9f9018b855
gogjoman <jirisukal08@gmail.com>
2025-11-07 13:35:05 +0100

    pridana uvodmsg a rozpracontan pridat zvire

--- UtulekProZvirata/UI/KonzoleUI.cs
+++ UtulekProZvirata/UI/KonzoleUI.cs
@@ -12,14 +12,31 @@ namespace UtulekProZvirata.UI
 
         
 
-        static string UI()
+        static string uvodmsg()
         {
+            //toto vypiš s console.write ne s console.writeline
             string uvodmsg = @"=-=-= Útulek pro zvířata =-=-= 
 1) Přidat zvíře 
 2) Vypsat všechna zvířata
 3) Vyhledat/filtrovat
-"; 
+4) Označit adopci
+5) Statistiky
+0) Konec
+
+Volba: "; 
             return uvodmsg;
+            
+        }
+
+        static string pridatzvire()
+        {
+            string pridatzvire_jmeno = "";
+            string pridatzvire_druh = "";
+            string pridatzvire_vek = "";
+
+            return pridatzvire_jmeno;
+            return pridatzvire_vek;
+            return pridatzvire_druh;
         }
     }
 }
