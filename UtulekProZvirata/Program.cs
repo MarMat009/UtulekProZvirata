a89109c464d4bdf7b68ac229b6b39ce4b47f400f
Martin Škoda <ahojda009@email.cz>
2025-11-13 10:29:23 +0100

    pridani zvirete

--- UtulekProZvirata/Program.cs
+++ UtulekProZvirata/Program.cs
@@ -13,26 +13,47 @@ namespace UtulekProZvirata
         static void Main(string[] args)
         {
             //start up
-
+            KonzoleUI konzoleui = new KonzoleUI();
             //import dat z excelu do listu
             List<Zvire> ListZvirat =  new List<Zvire>();
             ListZvirat.Add(new Zvire(1,"pes","pes",5,"M",true,"dnes","dobrý","nic"));
             //ListZvirat.Add(new Zvire());
             //ListZvirat.Add(new Zvire());
 
-            bool Bezi = false;
-            Console.WriteLine(ListZvirat.Count);
-            Console.ReadLine();
+            bool Bezi = true;
             //spustit while loop
             //volani KonzoleUI.cs
             while (Bezi)
             {
-                int rozhodnuti = 0;
-                //int rozhodnuti = int.Parse(KonzoleUI.UI());
+                //int rozhodnuti = 0;
+                int rozhodnuti = int.Parse(konzoleui.UI());
                switch (rozhodnuti)
                 {
                     case 1:
-                        //pridat zviře pomoci Zvire.cs
+                        string prefab = konzoleui.pridatzvire();
+                        string[] hodnoty = prefab.Split(',');
+                        bool kastrace = false;
+                        if (hodnoty[4] == "ano")
+                        {
+                            kastrace = true;
+                        }
+                        if (hodnoty[5] != "")
+                        {
+                            ListZvirat.Add(new Zvire(ListZvirat.Count + 1, hodnoty[0], hodnoty[1], int.Parse(hodnoty[2]), hodnoty[3], kastrace, hodnoty[5], hodnoty[6], hodnoty[7]));
+                        }
+                        else
+                        {
+                            ListZvirat.Add(new Zvire(ListZvirat.Count + 1, hodnoty[0], hodnoty[1], int.Parse(hodnoty[2]), hodnoty[3], kastrace,hodnoty[6], hodnoty[7]));
+                        }
+                        Console.WriteLine(ListZvirat[ListZvirat.Count - 1].ReturnId());
+                        Console.WriteLine(ListZvirat[ListZvirat.Count - 1].ReturnJmeno());
+                        Console.WriteLine(ListZvirat[ListZvirat.Count - 1].ReturnDruh());
+                        Console.WriteLine(ListZvirat[ListZvirat.Count - 1].ReturnVek());
+                        Console.WriteLine(ListZvirat[ListZvirat.Count - 1].ReturnPohlavi());
+                        Console.WriteLine(ListZvirat[ListZvirat.Count - 1].ReturnKastrovanost());
+                        //Console.WriteLine(ListZvirat[ListZvirat.Count - 1].);
+                        Console.WriteLine(ListZvirat[ListZvirat.Count - 1].ReturnZdravotniStav());
+                        Console.WriteLine(ListZvirat[ListZvirat.Count - 1].ReturnPoznamka());
                         break;
                     case 2:
                         //vypsat vsechna zvirata pomoci
