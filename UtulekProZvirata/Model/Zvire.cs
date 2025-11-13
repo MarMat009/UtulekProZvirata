f949fe4f91311581a6e713a4249b60ea72034ff0
MaroPlayCZ <marca.musil@gmail.com>
2025-11-13 09:20:57 +0100

    Přidání Metod pro vracení a změny informací

--- UtulekProZvirata/Model/Zvire.cs
+++ UtulekProZvirata/Model/Zvire.cs
@@ -17,11 +17,11 @@ namespace UtulekProZvirata.Model
         private string DatumPrijmu;
         private string ZdravotniStav;
         private string Poznamka;
+        private bool Adoptovan;
 
-        public Zvire(string Jmeno, string Druh, int Vek, string Pohlavi, bool Kastrovanost, string DatumPrijmu, string ZdravotniStav, string Poznamka)
+        public Zvire(int Id, string Jmeno, string Druh, int Vek, string Pohlavi, bool Kastrovanost, string DatumPrijmu, string ZdravotniStav, string Poznamka)
         {
-            int i = //podle listu z Program.cs
-            Id =  + 1;
+            this.Id = Id;
             this.Jmeno = Jmeno;
             this.Druh = Druh;
             this.Vek = Vek;
@@ -30,11 +30,11 @@ namespace UtulekProZvirata.Model
             this.DatumPrijmu = DatumPrijmu;
             this.ZdravotniStav = ZdravotniStav;
             this.Poznamka = Poznamka;
+            Adoptovan = false;
         }
-        public Zvire(string Jmeno, string Druh, int Vek, string Pohlavi, bool Kastrovanost, string ZdravotniStav, string Poznamka)
+        public Zvire(int Id, string Jmeno, string Druh, int Vek, string Pohlavi, bool Kastrovanost, string ZdravotniStav, string Poznamka)
         {
-            int i = ;
-            Id =  + 1;
+            this.Id = Id;
             this.Jmeno = Jmeno;
             this.Druh = Druh;
             this.Vek = Vek;
@@ -43,11 +43,73 @@ namespace UtulekProZvirata.Model
             DatumPrijmu = DateTime.Now.ToString("dd.MM.yyyy");
             this.ZdravotniStav = ZdravotniStav;
             this.Poznamka = Poznamka;
+            Adoptovan = false;
         }
+
+
+
+        // Metody pro vracení informací o zvířeti
 
         public override string ToString()
         {
-            return; //vratit vsechny atributy jako jeden string pro vypis
+            return "{Id}§{Jmeno}§{Druh}§{Vek}§{Pohlavi}§{Kastrovanost}§{DatumPrijmu}§{ZdravotniStav}§{Poznamka}";
+        }
+        public int ReturnId()
+        {
+            return Id;
+        }
+        public string ReturnJmeno()
+        {
+            return Jmeno;
+        }
+        public string ReturnDruh()
+        {
+            return Druh;
+        }
+        public int ReturnVek()
+        {
+            return Vek;
+        }
+        public string ReturnPohlavi()
+        {
+            return Pohlavi;
+        }
+        public bool ReturnKastrovanost()
+        {
+            return Kastrovanost;
+        }
+        public string ReturnZdravotniStav()
+        {
+            return ZdravotniStav;
+        }
+        public string ReturnPoznamka()
+        {
+            return Poznamka;
+        }
+
+
+
+        // Metody pro měnění informací o zvířeti
+
+        public void Zestarnout(int zmena)
+        {
+            Vek += zmena;
+        }
+        public void ZestarnoutORok()
+        {
+            Vek += 1;
+        }
+        public void Vykastrovat()
+        {
+            Kastrovanost = true;
+        }
+        public void ZmenZdravotniStav(string zmena)
+        {
+            ZdravotniStav = zmena;
+        }
+        public void ZmenPoznamka(string zmena)
+        {
+            Poznamka = zmena;
         }
     }
 }
