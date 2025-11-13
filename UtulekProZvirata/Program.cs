8a43835db1f4d744cc3651146eafe33d6e02fa67
Martin Škoda <ahojda009@email.cz>
2025-11-13 15:15:26 +0100

    tabulka vytvořena už se dá pracovat s excelem

--- UtulekProZvirata/Program.cs
+++ UtulekProZvirata/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using UtulekProZvirata.Model;
 using UtulekProZvirata.UI;
@@ -19,6 +20,50 @@ namespace UtulekProZvirata
             KonzoleUI konzoleui = new KonzoleUI();
 
             //import dat z excelu do listu
+            /*
+            string projectFolder = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory)
+                                          .Parent.Parent.Parent.FullName;
+
+            // 🧾 Název souboru
+            
+            string filePath = Path.Combine(projectFolder, "data.xlsx");
+
+            using (SpreadsheetDocument document = SpreadsheetDocument.Create(filePath, SpreadsheetDocumentType.Workbook))
+            {
+                // workbook = kniha
+                WorkbookPart workbookPart = document.AddWorkbookPart();
+                workbookPart.Workbook = new Workbook();
+
+                // worksheet = list
+                WorksheetPart worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+                SheetData sheetData = new SheetData();
+
+                // vytvoření řádku s jednou buňkou
+                Row row = new Row() { RowIndex = 1 };
+                Cell cell = new Cell()
+                {
+                    CellReference = "A1",
+                    DataType = CellValues.String,
+                    CellValue = new CellValue("Hello OpenXML!")
+                };
+
+                row.Append(cell);
+                sheetData.Append(row);
+                worksheetPart.Worksheet = new Worksheet(sheetData);
+
+                // přidání listu do sešitu
+                Sheets sheets = workbookPart.Workbook.AppendChild(new Sheets());
+                Sheet sheet = new Sheet();
+                sheet.Id = workbookPart.GetIdOfPart(worksheetPart);
+                sheet.SheetId = 1;
+                sheet.Name = "List1";
+                sheets.Append(sheet);
+
+                workbookPart.Workbook.Save();
+            }
+
+            Console.WriteLine("✅ Soubor úspěšně vytvořen!");
+            */
 
             List<Zvire> ListZvirat =  new List<Zvire>();
             ListZvirat.Add(new Zvire(1,"pes","pes",5,"M",true,"dnes","dobrý","nic"));
@@ -82,5 +127,14 @@ namespace UtulekProZvirata
                 //export do excelu
             
         }
+        static string GetValue(WorkbookPart wbPart, Cell cell)
+        {
+            if (cell.DataType != null && cell.DataType == CellValues.SharedString)
+            {
+                var sst = wbPart.SharedStringTablePart.SharedStringTable;
+                return sst.ChildElements[int.Parse(cell.InnerText)].InnerText;
+            }
+            return cell.InnerText;
+        }
     }
-}
+    }
